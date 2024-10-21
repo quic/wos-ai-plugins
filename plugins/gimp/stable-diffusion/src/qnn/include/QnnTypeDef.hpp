b27a072486e779194af69b726a66811abebf5b1f
Alok Kumar Pandey <quic_alopand@quicinc.com>
2024-10-21 10:44:26 +0530

    Add license
    Signed-off-by: Alok Kumar Pandey <quic_alopand@quicinc.com>

--- plugins/gimp/stable-diffusion/src/qnn/include/QnnTypeDef.hpp
+++ plugins/gimp/stable-diffusion/src/qnn/include/QnnTypeDef.hpp
@@ -1,3 +1,10 @@
+/*
+**************************************************************************************************
+* Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
+**************************************************************************************************
+*/
+
 #ifndef QNN_TYPE_DEF_H_
 #define QNN_TYPE_DEF_H_
 
