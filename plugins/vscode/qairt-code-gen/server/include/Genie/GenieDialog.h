216f5264dc6b49efa4f6809f9e91b9f4e131539a
Alok Kumar Pandey <alopand@qti.qualcomm.com>
2026-04-21 10:12:05 +0530

    add license headers
    Signed-off-by: Alok Kumar Pandey <quic_alopand@quicinc.com>
    Signed-off-by: Alok Kumar Pandey <alopand@qti.qualcomm.com>

--- plugins/vscode/qairt-code-gen/server/include/Genie/GenieDialog.h
+++ plugins/vscode/qairt-code-gen/server/include/Genie/GenieDialog.h
@@ -1,7 +1,9 @@
-// ---------------------------------------------------------------------
-// Copyright (c) 2026 Qualcomm Innovation Center, Inc. All rights reserved.
-// SPDX-License-Identifier: BSD-3-Clause
-// ---------------------------------------------------------------------
+/*
+**************************************************************************************************
+* Copyright (c) 2026 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
+**************************************************************************************************
+*/
 
 /**
  *  @file
